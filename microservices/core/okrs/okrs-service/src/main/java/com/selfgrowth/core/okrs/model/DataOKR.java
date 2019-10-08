883749477a956cde3d726f3362ba02ea8323581b
Dang Huu Canh <huucanh2092@gmail.com>
2019-10-08 08:50:27 +0700

    Add DataOKR.java

---
+++ microservices/core/okrs/okrs-service/src/main/java/com/selfgrowth/core/okrs/model/DataOKR.java
@@ -0,0 +1,13 @@
+package com.selfgrowth.core.okrs.model;
+
+import com.selfgrowth.model.okr.OKR;
+
+import java.util.List;
+
+public class DataOKR {
+    private List<OKR> data;
+
+    public DataOKR(List<OKR> data){
+        this.data = data;
+    }
+}
