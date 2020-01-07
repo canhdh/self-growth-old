9521f458942c5e46562cd1b6dc829563ca8a5bad
Dang Huu Canh <huucanh2092@gmail.com>
2020-01-07 15:44:34 +0700

    Delete registration-active folder

--- microservices/support/registration-activate/src/main/java/com/k001/selfgroth/registrationactivate/spring/ServiceConfig.java
+++
@@ -1,9 +0,0 @@
-package com.k001.selfgroth.registrationactivate.spring;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({ "org.baeldung.service" })
-public class ServiceConfig {
-}
