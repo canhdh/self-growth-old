458d351b0395fc4741a963c6b07c7fd4f0263754
Dang Huu Canh <huucanh2092@gmail.com>
2020-02-27 14:20:22 +0700

    Change database IP and Delete okr util on diary's branch

--- build-all.sh
+++ build-all.sh
@@ -3,17 +3,17 @@ cd microservices/composite/diary-composite-service
 # build diary-composite-service:0.1 image
 docker build -t diary-composite-service:0.1 .
 
-## change to diary-service directory
-#cd ../../core/diary
-## build diary-service:0.1 image
-#docker build -t diary-service:0.1 .
+# change to diary-service directory
+cd ../../core/diary
+# build diary-service:0.1 image
+docker build -t diary-service:0.1 .
 
 ## change to discovery-service directory
 #cd ../../support/discovery-services
 ## build discovery-service:0.1 image
 #docker build -t service-registry:0.1 .
-#
+##
 ## change to edge-service directory
 #cd ../edge-service
-## build api-gateway:0.1 image
+### build api-gateway:0.1 image
 #docker build -t api-gateway:0.1 .
